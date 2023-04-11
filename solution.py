c45a12bb353d73bbb48c84ee3d3b8ec85dc6caa3
Vladimir Grebnev <36560479+Grelodya@users.noreply.github.com>
2023-04-11 22:58:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,4 +11,29 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    def proportions_diff_z_stat_ind(i1, n1, i2, n2):
+      p1 = i1 / n1
+      p2 = i2 / n2 
+      P = float(p1*n1 + p2*n2) / (n1 + n2)
+      
+    def proportions_diff_z_test(z_stat, alternative = 'two-sided'):
+      if alternative not in ('two-sided', 'less', 'greater'):
+          raise ValueError("alternative not recognized\n"
+                           "should be 'two-sided', 'less' or 'greater'")
+
+      if alternative == 'two-sided':
+          return 2 * (1 - scipy.stats.norm.cdf(np.abs(z_stat)))
+
+      if alternative == 'less':
+          return scipy.stats.norm.cdf(z_stat)
+
+      if alternative == 'greater':
+          return 1 - scipy.stats.norm.cdf(z_stat)
+    
+    return (p1 - p2) / np.sqrt(P * (1 - P) * (1. / n1 + 1. / n2))
+  
+  if proportions_diff_z_test(proportions_diff_z_stat_ind(x_success, x_cnt, y_success, y_cnt)), 'less') < 0.1:
+    answer = True
+  else:
+    answer = False
+    return answer
